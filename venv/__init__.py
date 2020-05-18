e15523783bf8ae4884ef1a24f742b532238cc9bd
Karan1997-stack <madhyani86@gmail.com>
2020-05-18 14:48:04 +0530

    minor commit

--- venv/__init__.py
+++ venv/__init__.py
@@ -18,8 +18,8 @@ login_manager.login_message_category='info'
 app.config['MAIL_SERVER']= 'smtp.gmail.com'
 app.config['MAIL_PORT']=587
 app.config['MAIL_USE_TLS']= True
-app.config['MAIL_USERNAME']='madhyani86@gmail.com'
-app.config['MAIL_PASSWORD']='asskicker'
+app.config['MAIL_USERNAME']='anothertale28@gmail.com'
+app.config['MAIL_PASSWORD']='Itried123'
 mail = Mail(app)
 
 
